0429e9b0761ce50f8655da14cbf5700417b0d1f8
rockykartik <115865655+rockykartik@users.noreply.github.com>
2024-12-15 12:49:51 +0530

    Update bot.py

--- nexichat/modules/bot.py
+++ nexichat/modules/bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from pyrogram.types import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import google.generativeai as genai
 
 TOKEN = "7773367621:AAFAHG-Nqa-gDqogKYbqzv1f5yhUu8-v8j8"
